a2fcd8279780508cdb685bb63db3153c992aa17a
Amrito Ben Hasda <matheusjustinhasda@gmail.com>
2025-07-31 09:22:42 +0600

    create Product model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=100)
+    product_price = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product_name
